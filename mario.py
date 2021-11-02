98ef6049b40a3d2a7304f937669e44268d4ed406
75xor <75520201+75xor@users.noreply.github.com>
2021-11-02 13:50:29 +0900

    가속도 구현

--- mario.py
+++ mario.py
@@ -8,13 +8,13 @@ class mario:
         self.img = load_image('리소스\\mario.png')
         self.dir = 0
         self.frame = 0
+        self.speed=1
         self.d = 1
-        self.w = 40
+        self.w = 40  #캐릭터 크기
         self.h = 80
-        self.running = True
-        self.v = 7
-        self.m =2
-        self.j=0
+        self.running = True #게임 러닝
+        
+
 
 
     def handle_events(self):
@@ -29,68 +29,34 @@ class mario:
                 if event.key == SDLK_RIGHT:
                     self.d = 1
                     self.dir += 1
-                    self.state = 'run'
                 elif event.key == SDLK_LEFT:
-                    self.state = 'run'
                     self.d = 0
                     self.dir -= 1
                 elif event.key == SDLK_SPACE:
-                    self.state = 'jump'
-                    if self.j == 0:
-                        self.j = 1
-                    elif self.j  == 1:
-                        self.j =2
+                   pass
             elif event.type == SDL_KEYUP:
                 if event.key == SDLK_RIGHT:
                     self.dir -= 1
+                    self.speed = 1
                 elif event.key == SDLK_LEFT:
                     self.dir += 1
-                self.state = 'idle'
-
+                    self.speed = 1
 
     def update(self):
-        self.x += self.dir*5
-        if self.state == 'run':
-            self.frame = (self.frame+1) % 4
-            delay(0.01)
 
-        if self.state == 'jump':
-            if self.j > 0:
-                if self.j == 2:
-                    self.v = 7
+        if self.speed < 10:
+            self.speed += 0.2
 
-                # 역학공식 계산 (F). F = 0.5 * mass * velocity^2.
-                if self.v > 0:
-
-                    F = (0.5 * self.m * (self.v * self.v))
-                else:
-
-                    F = -(0.5 * self.m * (self.v * self.v))
+        self.x += self.dir * self.speed
 
 
-                self.y -= round(F)
-
-
-                self.v -= 1
-
-
-                if self.y > 110:
-                    self.y = 110
-                    self.j = 0
-                    self.state ='idle'
-                    self.v = 7
-
-
-
-
-
+        # if self.state == 'run':
+        #     self.frame = (self.frame+1) % 4
+        #     delay(0.01)
     def draw(self):
         if(self.state == 'idle'):
             self.img.clip_draw(0, 90 * self.d, 40, 80, self.x, self.y)
-        elif(self.state == 'run'):
-            self.img.clip_draw(self.frame * 50, 90 * self.d, 40, 80, self.x, self.y, self.w, self.h)
-        elif(self.state == 'jump'):
-            self.img.clip_draw(200, 90 * self.d, 40, 80, self.x, self.y, self.w, self.h)
+
 
 
 
